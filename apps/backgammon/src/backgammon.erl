9deadd82fb0d3f1114f366599dd5692f8a282032
Cezuriku <cezuriku@riseup.net>
2020-06-06 16:38:01 +0200

    Add possibility to join a game

--- apps/backgammon/src/backgammon.erl
+++ apps/backgammon/src/backgammon.erl
@@ -16,6 +16,7 @@
     {ok, nuk_game_engine_state:state()}.
 initialize(User, []) ->
     InitialBoard = #{
+        bar => #{},
         1 => {white, 2},
         6 => {black, 5},
         8 => {black, 3},
@@ -23,13 +24,14 @@ initialize(User, []) ->
         13 => {black, 5},
         17 => {white, 3},
         19 => {white, 5},
-        24 => {black, 2}
+        24 => {black, 2},
+        off => #{}
     },
 
     Username = nuk_user:get_username(User),
     StatePrivate = #{},
     StatePublic = #{board => InitialBoard},
-    StatePlayers = #{Username => #{}},
+    StatePlayers = #{Username => #{color => white}},
     State = nuk_game_engine_state:new(StatePrivate, StatePublic, StatePlayers),
     {ok, State};
 initialize(_User, OptionsOverride) when is_list(OptionsOverride) ->
@@ -37,7 +39,7 @@ initialize(_User, OptionsOverride) when is_list(OptionsOverride) ->
 
 player_join(User, State, _NukState) ->
     Username = nuk_user:get_username(User),
-    StatePlayer = #{},
+    StatePlayer = #{color => black},
     StateNew = nuk_game_engine_state:put_player(State, Username, StatePlayer),
     {ok, StateNew}.
 
@@ -67,9 +69,56 @@ start(State, NukState) ->
     {ok, await_turn,
         lists:nth(rand:uniform(2), nuk_game_state:get_players(NukState)), State}.
 
+turn(User, {_Dices, Moves}, State, NukState) when is_list(Moves) ->
+    #{board := Board} = StatePublic = nuk_game_engine_state:get_public(State),
+    #{color := Color} =
+        nuk_game_engine_state:get_player(State, nuk_user:get_username(User)),
+    [Move | _] = Moves,
+    {Origin, Target} = Move,
+    {ok, BoardNew1} = valid_origin(Board, Origin, Color),
+    {ok, BoardNew2} = valid_target(BoardNew1, Target, Color),
+    NextTurns = lists:delete(User, nuk_game_state:get_players(NukState)),
+    NukStateNew =
+        nuk_game_engine_state:set_public(NukState, StatePublic#{board := BoardNew2}),
+    {ok, await_turn, NextTurns, NukStateNew};
 turn(_User, _Turn, _State, _NukState) ->
     {error, invalid_turn, "Invalid"}.
 
 finish(_State, _NukState) ->
     ok.
 
+%%====================================================================
+%% Internal functions
+%%====================================================================
+valid_origin(#{bar := Bar} = Board, bar, Color) ->
+    case Bar of
+        #{Color := 1} ->
+            {ok, Board#{bar := maps:remove(Color, Bar)}};
+        #{Color := Nb} ->
+            {ok, Board#{bar := Bar#{Color := Nb - 1}}};
+        _ ->
+            error
+    end;
+valid_origin(Board, Pos, Color) when is_integer(Pos) ->
+    case Board of
+        #{Pos := {Color, 1}} ->
+            {ok, maps:remove(Pos, Board)};
+        #{Pos := {Color, Nb}} ->
+            {ok, Board#{Pos := Nb - 1}};
+        _ ->
+            error
+    end.
+
+valid_target(#{off := Off} = Board, off, Color) ->
+    Nb = maps:get(Color, Off, 0) + 1,
+    {ok, Board#{off => Off#{Color => Nb}}};
+valid_target(Board, Pos, Color) ->
+    case Board of
+        #{Pos := {Color, Nb}} ->
+            {ok, Board#{Pos => {Color, Nb + 1}}};
+        #{Pos := {OppositeColor, 1}, off := Off} ->
+            Nb = maps:get(OppositeColor, Off, 0) + 1,
+            {ok, Board#{off => Off#{OppositeColor => Nb}, Pos => {Color, 1}}};
+        _ ->
+            error
+    end.
--- apps/backgammon/test/backgammon_game_SUITE.erl
+++ apps/backgammon/test/backgammon_game_SUITE.erl
@@ -17,7 +17,8 @@
 -export([
     backgammon_game_register_get/1,
     backgammon_game_start_with_options_invalid/1,
-    backgammon_game_start/1
+    backgammon_game_start/1,
+    backgammon_game_start_and_join/1
 ]).
 
 %%====================================================================
@@ -27,7 +28,8 @@ all() ->
     [
         backgammon_game_register_get,
         backgammon_game_start_with_options_invalid,
-        backgammon_game_start
+        backgammon_game_start,
+        backgammon_game_start_and_join
     ].
 
 init_per_suite(Config) ->
@@ -78,3 +80,24 @@ backgammon_game_start(_) ->
 
     % create a game
     {ok, _GameSessionId} = nuk_games:create(UserSessionId1, "Backgammon", []).
+
+backgammon_game_start_and_join(_) ->
+    % register game
+    Game = nuk_game:new("Backgammon", backgammon, 2, 2),
+    ok = nuk_games:register(Game),
+
+    % create and login user
+    User1 = nuk_user:new("User1", "Pass1"),
+    ok = nuk_users:put(User1),
+    {ok, UserSessionId1} = nuk_users:login("User1", "Pass1"),
+
+    % create a game
+    {ok, GameSessionId} = nuk_games:create(UserSessionId1, "Backgammon", []),
+
+    % create and login user2
+    User2 = nuk_user:new("User2", "Pass2"),
+    ok = nuk_users:put(User2),
+    {ok, UserSessionId2} = nuk_users:login("User2", "Pass2"),
+
+    % Join a game
+    ok = nuk_games:join(GameSessionId, UserSessionId2).
